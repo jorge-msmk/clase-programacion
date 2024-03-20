e31a063b220473e57413fefe3a6c9a62d337d196
Samantha Fernández <samanthaangelesfp@msmk.university>
2024-03-20 11:02:10 +0100

    type script

---
+++ samantha/script.ts
@@ -0,0 +1,9 @@
+const nombre: string = "Samantha";
+const numero: number = 4 ;
+const arriveNumero: number[] = [3,25,1000,-5,6];
+const trueFalse: boolean = true;
+const trueFalseArray: boolean[] = [true,false,true,false,false] ;
+let datoVacio: Number;
+
+datoVacio = 5;
+datoVacio = 28;
